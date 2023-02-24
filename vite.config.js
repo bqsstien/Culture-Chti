d437ae452d3bf7cc62e8bb116dcb2b38d730c2f8
Sebastien verslype <sebastien.verslype@gmail.com>
2023-02-24 11:44:24 +0100

    ajout des packages postcss/autoprefixer/vite-plugin-purgecss et modification des fichiers pour mettre em prod

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,32 @@
-const path = require("path");
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import htmlPurge from 'vite-plugin-purgecss';
 
-export default {
-    root: path.resolve(__dirname, "src"),
+export default defineConfig({
+    root: resolve(__dirname, "src"),
     resolve: {
         alias: {
-            "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+            "~bootstrap": resolve(__dirname, "node_modules/bootstrap"),
         },
     },
     server: {
         port: 8080,
         hot: true,
     },
-};
+    plugins: [
+        htmlPurge([htmlPurge()])
+    ],
+    build: {
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, './src/index.html'),
+                carnaval: resolve(__dirname, './src/carnaval.html'),
+                patois: resolve(__dirname, './src/patois.html'),
+                beffrois: resolve(__dirname, './src/beffrois.html'),
+            }
+        }
+    }
+});
+
+
+
